d0b9ab5275c9c46e57cedfd8d11ed0c7b3d84e07
Vivek gupta <me.vivekgupta9@gmail.com>
2023-09-07 20:28:33 +0530

    same code but updated

--- index.js
+++ index.js
@@ -1,50 +1,3 @@
-// const express = require("express");
-// const path = require("path");
-// const bodyParser = require("body-parser");
-// const fs = require("fs");
-// // const router = require("./UserRoutes");
-
-// const app = express();
-// const port = 4000;
-// let x;
-// app.use(bodyParser.urlencoded({ extended: false }));
-// // app.use(express.json());
-// // app.use("/api/v1", router);
-
-// app.get("/", (req, res) => {
-// 	fs.readFile("read.txt", "utf-8", (err, data) => {
-// 		res.send(`${data}<form action="/" method="POST">
-//     <input placeholder="Enter Your Message" type="text" name="message" id="message" />
-//     <input type="submit" value="Login" />
-//   </form>`);
-// 	});
-// });
-
-// app.post("/", (req, res) => {
-// 	fs.appendFile("read.txt", `${req.body.message}  `, (err) => {
-// 		err ? console.log(err) : res.redirect("/");
-// 	});
-// 	// console.log(req.body.message);
-// });
-
-// app.get("/login", (req, res) => {
-// 	res.send(`<form onsubmit="localStorage.setItem("username", document.getElementById('name').value)" action="/login" method="POST"><h1>REGISTER</h1>
-//     <input placeholder="Enter Your Name" type="text" name="name" id="name" />
-//     <input type="submit" value="Login" />
-//   </form>`);
-// });
-// app.post("/login", (req, res) => {
-// 	fs.appendFile("read.txt", `${req.body.name}-`, (err) => {
-// 		err ? console.log(err) : res.redirect("/");
-// 	});
-// 	// console.log(req.body.name);
-// 	// console.log(req.body.message);
-// });
-
-// app.listen(port, () => {
-// 	console.log(`Server is Working on port: ${port}`);
-// });
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
@@ -55,7 +8,7 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-	fs.readFile("read.txt", "utf-8", (err, data) => {
+	fs.readFile("read.txt", (err, data) => {
 		if (err) {
 			console.log(err);
 			data = "No chat exist";
